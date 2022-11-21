f3ed96601c3de2f981620dadff024a660cb7d22f
Fikayo Jetawo <jetawof@gmail.com>
2022-11-21 17:13:05 +0100

    navBar and Overview

--- amen-jet/src/components/Home.js
+++ amen-jet/src/components/Home.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-wrap mt-36 pl-5 justify-center w-full">
+      <img
+        className="rounded-3xl "
+        src="https://stella-hospital.netlify.app/img/Medicine.gif"
+        alt="pic"
+      />
+      <div className="p-5 flex flex-col lg:w-2/4 lg:pl-36">
+        <h1 className="text-6xl p-5 font-serif">AMEN JET MEDICAL SERVICES</h1>
+        <p className="text-3xl p-5 font-mono">
+          Bringing quality health care to your door step.
+        </p>
+        <button className=" p-5 rounded-3xl bg-blue-500 font-serif text-center hover:bg-inherit hover:border text-2xl text-white">
+          Book Appointment
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- amen-jet/src/components/NavBar.js
+++ amen-jet/src/components/NavBar.js
@@ -1,7 +1,31 @@
 import React from "react";
-
+import "./style.css";
 function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div className="absolute w-full h-32 top-0 flex flex-col bg-black justify-end">
+      <img
+        className=" w-full top-0 left-0 h-32 opacity-40"
+        alt="#"
+        src="https://www.freewebheaders.com/wp-content/gallery/healthcare_1/stethoscope-medical-check-up-website-header.jpg"
+      ></img>
+      <h2 className="absolute text-white text-4xl font-mono top-2 left-10">
+        Amen Jet
+      </h2>
+      <h2 className="absolute text-white text-4xl top-2 right-10">&#9776;</h2>
+      <ul
+        id="Menu"
+        className="absolute text-white flex md:text-2xl sm:lg font-serif space-x-6 p-5 text-xl z-index-1 w-full justify-cente"
+      >
+        <li>Overview</li>
+        <li>Services</li>
+        <li>Contact</li>
+        <li>Reviews</li>
+        <li>Gallery</li>
+        <li>Contact</li>
+        <li className="hidden">Menu</li>
+      </ul>
+    </div>
+  );
 }
 
 export default NavBar;

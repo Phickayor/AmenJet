e54da318a2907e7275accdd2e2542dcb31f0c44f
Fikayo Jetawo <jetawof@gmail.com>
2022-11-24 22:05:39 +0100

    Adding socials to contact

--- amen-jet/src/components/Contact.js
+++ amen-jet/src/components/Contact.js
@@ -1,6 +1,17 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faMailBulk,
+  faPhone
+} from "@fortawesome/free-solid-svg-icons";
+import { faInstagram, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 
 function Contact() {
+  const whatsapp = <FontAwesomeIcon icon={faWhatsapp} />;
+  const instagram = <FontAwesomeIcon icon={faInstagram} />;
+  const mail = <FontAwesomeIcon icon={faEnvelope} />;
+  const phone = <FontAwesomeIcon icon={faPhone} />;
   return (
     <div
       id="contact"
@@ -15,15 +26,16 @@ function Contact() {
       <div
         data-aos="fade-up"
         data-aos-duration="3000"
-        className="w-9/12 m-10 bg-white text-black shadow-2xl  p-5 rounded-3xl hover:bg-blue-500 hover:text-white"
+        className="flex w-full m-10 flex-wrap sm:flex-nowrap justify-center"
       >
-        <form>
+        <form className=" bg-white w-9/12 m-5 text-black shadow-2xl p-5 rounded-3xl hover:bg-blue-500 hover:text-white">
           <label className="p-5 font-mono text-3xl">E-mail</label>
           <br />
           <input
             type="email"
             className="w-9/12 m-5 bg-white font-serif text-black text-center focus:outline-none text-2xl shadow-2xl p-5 rounded-3xl border-4 border-black"
             placeholder="Your Mail..."
+            required
           />
           <br />
           <label className="p-5 font-mono text-3xl">Message</label>
@@ -32,11 +44,27 @@ function Contact() {
             type="text"
             className="w-9/12 m-5 bg-white font-serif text-black text-center focus:outline-none text-2xl shadow-2xl p-5 rounded-3xl border-4 border-black"
             placeholder="Drop your message here..."
+            required
           />
-          <button className="lg:w-2/4 m-5 w-9/12 bg-white text-black text-center focus:outline-none text-2xl shadow-2xl p-5 font-mono rounded-3xl border-4 border-black hover:bg-blue-700 hover:text-white">
+          <button className="lg:w-2/4 m-5 w-9/12 bg-white text-black text-center focus:outline-none text-2xl shadow-2xl p-5 font-mono rounded-3xl border-4 border-black hover:bg-blue-700 hover:text-white hover:lg:w-9/12">
             Submit
           </button>
         </form>
+
+        <ul className=" p-5 bg-white m-5 text-black rounded-2xl flex justify-center sm:flex-col ">
+          <a href=".">
+            <li className="text-6xl m-5 hover:text-blue-500">{whatsapp}</li>
+          </a>
+          <a href=".">
+            <li className="text-6xl m-5 hover:text-blue-500">{instagram}</li>
+          </a>
+          <a href=".">
+            <li className="text-5xl m-5 hover:text-blue-500">{phone}</li>
+          </a>
+          <a href=".">
+            <li className="text-5xl m-5 hover:text-blue-500">{mail}</li>
+          </a>
+        </ul>
       </div>
     </div>
   );
